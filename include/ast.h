3b3977caaa142dea2352a9754f641b7460a1200f
Chenhao Xue <xch927027@pku.edu.cn>
2022-09-30 19:22:37 +0800

    ast for const decl

--- include/ast.h
+++ include/ast.h
@@ -3,9 +3,11 @@
 #include <cstring>
 #include <iostream>
 #include <memory>
+#include <vector>
 
 #include "irgen.h"
 
+// Base class of AST
 class BaseAST {
    public:
     virtual ~BaseAST() = default;
@@ -13,9 +15,44 @@ class BaseAST {
     virtual void dump_koopa(IRGenerator &irgen, std::ostream &out) const = 0;
 };
 
+// CompUnit       ::= FuncDef
 class CompUnitAST : public BaseAST {
    public:
     std::unique_ptr<BaseAST> func_def;
+
+    void dump_koopa(IRGenerator &irgen, std::ostream &out) const override;
+};
+
+// Decl          ::= ConstDecl | VarDecl
+// ConstDecl     ::= "const" BType ConstDef {"," ConstDef} ";"
+// VarDecl       ::= BType VarDef {"," VarDef} ";"
+class DeclAST : public BaseAST {
+   public:
+    bool is_const;
+    std::string btype;
+    std::vector<std::unique_ptr<DeclDefAST>> defs;
+
+    void dump_koopa(IRGenerator &irgen, std::ostream &out) const override;
+};
+
+// ConstDef      ::= IDENT "=" ConstInitVal
+// VarDef        ::= IDENT | IDENT "=" InitVal
+class DeclDefAST : public BaseAST {
+   public:
+    bool is_const;
+    std::string ident;
+    std::unique_ptr<BaseAST> init_val;
+
+    void dump_koopa(IRGenerator &irgen, std::ostream &out) const override;
+};
+
+// ConstInitVal  ::= ConstExp
+// InitVal       ::= Exp
+// TODO: This will be expanded for array
+class InitValAST : public BaseAST {
+   public:
+    bool is_const;
+    std::unique_ptr<BaseAST> exp;
 
     void dump_koopa(IRGenerator &irgen, std::ostream &out) const override;
 };
@@ -47,8 +84,11 @@ class StmtAST : public BaseAST {
     void dump_koopa(IRGenerator &irgen, std::ostream &out) const override;
 };
 
+// Exp           ::= LOrExp
+// ConstExp      ::= Exp
 class ExpAST : public BaseAST {
    public:
+    bool is_const;
     std::unique_ptr<BaseAST> binary_exp;
 
     void dump_koopa(IRGenerator &irgen, std::ostream &out) const override;
@@ -59,6 +99,12 @@ typedef enum {
     BINARY_EXP_AST_TYPE_1       // l_exp op r_exp
 } binary_exp_ast_type;
 
+// MulExp        ::= UnaryExp | MulExp ("*" | "/" | "%") UnaryExp;
+// AddExp        ::= MulExp | AddExp ("+" | "-") MulExp;
+// RelExp        ::= AddExp | RelExp ("<" | ">" | "<=" | ">=") AddExp;
+// EqExp         ::= RelExp | EqExp ("==" | "!=") RelExp;
+// LAndExp       ::= EqExp | LAndExp "&&" EqExp;
+// LOrExp        ::= LAndExp | LOrExp "||" LAndExp;
 class BinaryExpAST : public BaseAST {
    public:
     binary_exp_ast_type type;
@@ -74,6 +120,7 @@ typedef enum {
     UNARY_EXP_AST_TYPE_1,      // unary_op unary_exp
 } unary_exp_ast_type;
 
+// UnaryExp      ::= PrimaryExp | UnaryOp UnaryExp;
 class UnaryExpAST : public BaseAST {
    public:
     unary_exp_ast_type type;
