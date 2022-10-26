fc3e2c811576e4bf737f88af26a04c2c387174b2
Chenhao Xue <xch927027@pku.edu.cn>
2022-10-26 14:04:25 +0800

    main: multiple runs

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
-
 #include <cassert>
 #include <cstdio>
 #include <cstring>
 #include <fstream>
 #include <iostream>
 #include <memory>
-
 #include "ast.h"
 #include "tcgen.h"
 
@@ -28,28 +26,42 @@ int main(int argc, const char *argv[]) {
     auto ret = yyparse(ast);
     assert(!ret);
 
-    // dumping koopa IR
     std::fstream out;
-    const std::string koopa_log = ".koopa.txt";
-    if (mode == "-koopa")
-        out.open(output, ios::out);
-    else
-        out.open(koopa_log, ios::out);
+    std::string koopa_file = "a.koopa";
+    std::string assembly_file = "a.S";
+
+    // ast -> IR
+    out.open(koopa_file, ios::out);
     assert(out.is_open());
     IRGenerator irgen;
     ast->dump_koopa(irgen, out);
     out.close();
 
-    // dumping riscv
-    if (mode == "-riscv") {
-        out.open(output, std::ios::out);
-        assert(out.is_open());
-        TargetCodeGenerator backend(koopa_log.c_str(), out);
-        int ret = backend.dump_riscv();
-        assert(!ret);
-        out.close();
+    // IR -> riscv assembly
+    out.open(assembly_file, ios::out);
+    assert(out.is_open());
+    TargetCodeGenerator tcgen(koopa_file.c_str(), out);
+    assert(!tcgen.dump_riscv());
+    out.close();
+
+    // copy to output
+    std::fstream in;
+    if (mode == "-koopa") {
+        in.open(koopa_file, ios::in);
+    } else if (mode == "-riscv" || mode == "-perf") {
+        in.open(assembly_file, ios::in);
+    } else {
+        std::cerr << "Compiler: unrecognized mode " << mode << std::endl;
+        return 1;
     }
-    std::cerr << "Dumping Finished!" << std::endl;
+    out.open(output, ios::out);
+    assert(in);
+    assert(out);
+    out << in.rdbuf();
+    in.close();
+    out.close();
+
+    std::cerr << "Compiler: Finished!" << std::endl;
 
     return 0;
 }
